0238f64085248850004f6730d6ff99ef03b36a22
pranavadith7 <pranavadith7@gmail.com>
2020-12-05 22:31:08 +0530

    changed login to Login

--- home.php
+++ home.php
@@ -39,7 +39,7 @@
     <?php
       session_start();
       if(!isset($_SESSION['rollNo'])) {
-        header('location: login.php');
+        header('location: Login.php');
       }
       // Connection
       //$conn= mysqli_connect('localhost', 'root', '','leave');
